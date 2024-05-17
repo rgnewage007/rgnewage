33969cd4ad22807f094d90fa75c297433c814c8a
rgnewage007 <165745639+rgnewage007@users.noreply.github.com>
2024-05-16 19:48:41 -0600

    Portafolio o portfolio
    ok

---
+++ _next/static/rJhr-0q-wX4VJ9F0sVhcr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/550-4c3b094fc36db39c.js","static/chunks/pages/index-47394e0a5878bc8b.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
